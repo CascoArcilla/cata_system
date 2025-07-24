89e83ae8db23686a8a96e70a077d189e0a00926a
CascoArcilla <normon1230@gmail.com>
2025-07-24 13:18:15 -0600

    Manejo de errores en primer formulario de configuracion

--- tecnicas/views/configuracion_panel.py
+++ tecnicas/views/configuracion_panel.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest
+from django.urls import reverse
 from ..forms import SesionFirtsForm
 from ..models import TipoTecnica
 
@@ -11,16 +12,16 @@ def configuracionPanel(req: HttpRequest):
         else:
             return render(req, "tecnicas/configuracion-panel.html", { "form_sesion": form })
     elif req.method == "GET":
-        if req.GET["id_tecnica"]:
-            try:
-                tecnica = TipoTecnica.objects.get(pk=int(req.GET["id_tecnica"]))
-            except (ValueError, TipoTecnica.DoesNotExist):
-                
-                pass
-        else:
-            redirect("/seleccion-tecnica")
-            pass
-        
-        form_sesion = SesionFirtsForm()
+        try:
+            id_tecnica = req.GET["id_tecnica"]
+            tecnica = TipoTecnica.objects.get(pk=id_tecnica)
+        except KeyError:
+            return redirect(reverse("cata_system:seleccion_tecnica") + "?error=tecnica_no_establecida")
+        except (ValueError, TipoTecnica.DoesNotExist):
+            return redirect(reverse("cata_system:seleccion_tecnica") + "?error=tecnica_no_establecida")
 
-        return render(req, "tecnicas/configuracion-panel.html", { "form_sesion": form_sesion })
+        if tecnica:
+            form_sesion = SesionFirtsForm()
+            return render(req, "tecnicas/configuracion-panel.html", { "form_sesion": form_sesion })
+        else:
+            return redirect(reverse("cata_system:seleccion_tecnica") + "?error=la_tecnica_no_existe")
--- tecnicas/views/seleccion_tecnica.py
+++ tecnicas/views/seleccion_tecnica.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
 from ..controllers  import TecnicaController
 
-def selecionTecnica(req):
+def selecionTecnica(req:HttpRequest):
     tipos = TecnicaController.getTypesTechnique()
-    return render(req, "tecnicas/seleccion-tecnica.html", context={"tipos":tipos})
+    try:
+        error : str = req.GET["error"]
+        error = error.replace("_", " ")
+        error = error.capitalize()
+
+        return render(req, "tecnicas/seleccion-tecnica.html", context={"tipos":tipos, "error":error})
+    except KeyError:
+        return render(req, "tecnicas/seleccion-tecnica.html", context={"tipos":tipos})
