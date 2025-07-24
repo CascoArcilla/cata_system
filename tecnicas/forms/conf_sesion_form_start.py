bc22ead5ce437960c155640a382f58aba7bd7d99
CascoArcilla <normon1230@gmail.com>
2025-07-24 11:18:11 -0600

    Validacion primer formulario configuracion de sesion

--- tecnicas/forms/conf_sesion_form_start.py
+++ tecnicas/forms/conf_sesion_form_start.py
@@ -7,7 +7,7 @@ class SesionFirtsForm(forms.Form):
         "class": "bg-gray-200 border-b-1 text-center w-full p-1",
         "name": "nombre_sesion",
         "placeholder": "Ej. Mermelada de mango picante"
-    }), required=True)
+    }), required=False)
 
     numero_productos = forms.IntegerField(widget=forms.NumberInput(attrs={
         "class": "bg-gray-200 p-1 border-b-1 text-center w-full",
@@ -24,10 +24,34 @@ class SesionFirtsForm(forms.Form):
         "placeholder": "Solo números"
     }), required=True)
 
-    tipo_escala = forms.ModelChoiceField(queryset=TipoEscala.objects.all(), required=True, widget=forms.RadioSelect(attrs={
+    tipo_escala = forms.ModelChoiceField(queryset=TipoEscala.objects.all(), widget=forms.RadioSelect(attrs={
         "class":"uppercase text-lg tracking-wider font-medium p-2 px-4 active:px-5 transition-all rounded-xl bg-blue-500 text-white",
-    }))
+    }), required=True, initial=TipoEscala.objects.first())
 
     tamano_escala = forms.IntegerField(widget=forms.NumberInput(attrs={
         "class": "bg-gray-200 p-1 border-b-1 text-center w-full",
-    }), required=True, min_value=5, max_value=9)
+    }), required=True, min_value=5)
+
+    def clean(self):
+        data_clean = super().clean()
+
+        sizes_estruturada = [5, 7, 9]
+        sizes_continua = [9, 12, 15]
+
+        escala = data_clean.get("tipo_escala")
+
+        if escala and not isinstance(escala, TipoEscala):
+            try:
+                escala = TipoEscala.objects.get(pk=escala)
+            except (ValueError, TipoEscala.DoesNotExist):
+                self.add_error("tipo_escala", "Escala no valida")
+                return data_clean
+
+        tamano_escala = data_clean.get("tamano_escala")
+
+        if escala.nombre_escala == "estructurada" and not sizes_estruturada.__contains__(tamano_escala):
+            self.add_error("tamano_escala", "El tamaño de la escala no aplica")
+        elif escala.nombre_escala == "continua" and not sizes_continua.__contains__(tamano_escala):
+            self.add_error("tamano_escala", "El tamaño de la escala no aplica")
+        else:
+            self.add_error("tipo_escala", "Escala no valida")
--- tecnicas/views/configuracion_panel.py
+++ tecnicas/views/configuracion_panel.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
-from ..controllers import EscalaController
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
 from ..forms import SesionFirtsForm
+from ..models import TipoTecnica
 
-def configuracionPanel(req):
-    # escalas = EscalaController.getTypesScale()
-    form_sesion = SesionFirtsForm()
+def configuracionPanel(req: HttpRequest):
+    if req.method == "POST":
+        form = SesionFirtsForm(req.POST)
+        if form.is_valid():
+            return redirect("/cata")
+        else:
+            return render(req, "tecnicas/configuracion-panel.html", { "form_sesion": form })
+    elif req.method == "GET":
+        if req.GET["id_tecnica"]:
+            try:
+                tecnica = TipoTecnica.objects.get(pk=int(req.GET["id_tecnica"]))
+            except (ValueError, TipoTecnica.DoesNotExist):
+                
+                pass
+        else:
+            redirect("/seleccion-tecnica")
+            pass
+        
+        form_sesion = SesionFirtsForm()
 
-    return render(req, "tecnicas/configuracion-panel.html", { "form_sesion": form_sesion })
+        return render(req, "tecnicas/configuracion-panel.html", { "form_sesion": form_sesion })
