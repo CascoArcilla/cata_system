ea26a8af4a903ebd221dfb17ea29de46e8cdbaaa
CascoArcilla <normon1230@gmail.com>
2025-08-09 13:12:38 -0600

    Se impleneta la funcion de actualizar un catador en la busqueda

--- tecnicas/views/catador_search.py
+++ tecnicas/views/catador_search.py
@@ -1,25 +1,56 @@
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.db import Error
+from django.core.exceptions import ObjectDoesNotExist
 from ..forms import CatadorForm
 from ..models import Catador
 
 def searchCatador(req : HttpRequest):
     if req.method == "GET":
+        context = {}
+
         try:
             user = req.GET["user"]
         except:
             user = False
+
+        if not user:
+            return render(req, "tecnicas/catador-buscar.html")
         
+        try:
+            catador = Catador.objects.get(usuarioCatador=user)
+            context["form_catador"] = CatadorForm(instance=catador)
+        except Catador.DoesNotExist:
+            context["error"] = "usuario no encontrado"
+
+        return render(req, "tecnicas/catador-buscar.html", context)
+    elif req.method == "POST":
         context = {}
-        if user:
-            try:
-                catador = Catador.objects.get(usuarioCatador=user)
-                print(catador.fechaNacimiento)
-                context["form_catador"] = CatadorForm(instance=catador)
-            except Error:
-                context["error"] = "usuario no encontrado"
 
+        try:
+            infoCata = req.POST
+        except:
+            infoCata = False
+
+        nameUser = req.POST["usuarioCatador"]
+
+        if not infoCata:
+            context["error"] = "ha ocurrido un error en recueperar los datos"
             return render(req, "tecnicas/catador-buscar.html", context)
+        
+        user = get_object_or_404(Catador, usuarioCatador=nameUser)
+        
+        modelForm = CatadorForm(infoCata, instance=user)
 
-        return render(req, "tecnicas/catador-buscar.html")
+        try:
+            if not modelForm.is_valid():
+                context["error"] = "ha ocurrido un error en guardar los datos"
+                return render(req, "tecnicas/catador-buscar.html", context)
+            modelForm.save()
+            context["form_catador"] = modelForm
+            context["message"] = "usuario actualizado"
+        except:
+            context["form_catador"] = modelForm
+            context["error"] = "ha ocurrido un error en guardar los datos"
+
+        return render(req, "tecnicas/catador-buscar.html", context)
