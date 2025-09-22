02472baa52c611f646496d9b70a790f5a1cc452a
CascoArcilla <normon1230@gmail.com>
2025-09-22 12:27:08 -0600

    Se crea superusuario en shell local

--- create_superuser.py
+++ create_superuser.py
@@ -3,9 +3,9 @@ import os
 
 User = get_user_model()
 
-username = os.getenv("SUPERUSER_USERNAME", "admin")
-email = os.getenv("SUPERUSER_EMAIL", "admin@example.com")
-password = os.getenv("SUPERUSER_PASSWORD", "admin123")
+username = os.getenv("SUPERUSER_USERNAME")
+email = os.getenv("SUPERUSER_EMAIL")
+password = os.getenv("SUPERUSER_PASSWORD")
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
