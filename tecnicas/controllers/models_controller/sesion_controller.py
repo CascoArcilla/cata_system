ba8562aae11b2e5806218761331fe02564967b5b
CascoArcilla <normon1230@gmail.com>
2026-01-08 13:22:49 -0600

    Correccion en listado de sesiones sensoriales, primero los activos

--- tecnicas/controllers/models_controller/sesion_controller.py
+++ tecnicas/controllers/models_controller/sesion_controller.py
@@ -67,7 +67,7 @@ class SesionController():
                 "tecnica__tipo_tecnica__nombre_tecnica",
                 "tecnica__id_estilo__nombre_estilo"
             )
-            .order_by("-fechaCreacion")
+            .order_by("-activo", "-fechaCreacion")
         )
 
         paginator = Paginator(queryset, elements_by_page)
