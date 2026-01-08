20c401b6c308a9fd19fcc7345c1b080c17348420
CascoArcilla <normon1230@gmail.com>
2026-01-08 14:28:15 -0600

    Se establce la zona horaria por defecto para la Ciudad de Mexico

--- cata_system/settings.py
+++ cata_system/settings.py
@@ -134,13 +134,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'es-mx'
 USE_I18N = True
 
 USE_TZ = True
+TIME_ZONE = 'America/Mexico_City'
 
 
 # Static files (CSS, JavaScript, Images)
